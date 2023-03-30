6cae23eb30188bf896a0e31dba1e8c2d4b3ace5d
THIEBAUT Hugo <hugo.thiebaut6@etu.univ-lorraine.fr>
2023-03-30 16:35:54 +0200

    gpt4

--- lib/queryApi.ts
+++ lib/queryApi.ts
@@ -2,16 +2,16 @@ import openai from "./chatgpt";
 
 const query = async (prompt: string, chatId: string, model: string) => {
   const res = await openai
-    .createCompletion({
+    .createChatCompletion({
       model,
-      prompt,
+      messages: [{ role: "user", content: prompt }],
       temperature: 0.9,
       top_p: 1,
       max_tokens: 1000,
       frequency_penalty: 0,
       presence_penalty: 0,
     })
-    .then((res) => res.data.choices[0].text)
+    .then((res) => res.data.choices[0].message?.content)
     .catch(
       (err) =>
         `ChatGPT n'a pas pu trouver de réponse à cela! (Error: ${err.message})`
